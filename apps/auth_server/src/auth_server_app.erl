43075cc804034e2b1f3a840a3679fdd09dac5444
Liam Tengelis <ltengelis@gmail.com>
2020-01-19 10:44:33 -0800

    initial commit

---
+++ apps/auth_server/src/auth_server_app.erl
@@ -0,0 +1,34 @@
+%%%-------------------------------------------------------------------
+%% @doc auth_server public API
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(auth_server_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    {ok, RuntimeArgs} = application:get_env(auth_server, runtime_args),
+    #{listen_port := ListenPort} = RuntimeArgs,
+    Dispatch = cowboy_router:compile([
+                                      {'_',
+                                       routes()}
+                                     ]),
+    logger:info("starting listener on ~p", [ListenPort]),
+    {ok, _} = cowboy:start_clear(auth_http_listener,
+                                 [{port, ListenPort}],
+                                 #{env => #{dispatch => Dispatch}}),
+    auth_server_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
+routes() ->
+    [
+     {"/test", base_handler,
+      [base_handler, say_hi]
+     }
+    ].
---
+++ apps/auth_server/src/base_handler.erl
@@ -0,0 +1,72 @@
+-module(base_handler).
+
+%% cowboy
+-export([
+         init/2,
+         allowed_methods/2,
+         content_types_provided/2,
+         content_types_accepted/2,
+         json_to_input/2,
+         res_to_json/2
+        ]).
+
+%% handler fns
+-export([
+         say_hi/1, say_hi/2
+        ]).
+
+
+-record(state, {op=[nomod, nofun],
+                data=none}).
+
+%% cowboy
+init(Req, Opts) ->
+    [OpMod, OpFn] = Opts,
+    State = #state{op=[OpMod, OpFn]},
+    {cowboy_rest, Req, State}.
+
+allowed_methods(Req, #state{op=[Mod, Fn]} = State) ->
+    Methods = erlang:apply(Mod, Fn, [allowed_methods]),
+    {Methods, Req, State}.
+
+content_types_accepted(Req, State) ->
+    {[
+      {<<"application/json">>, json_to_input},
+      {undefined, json_to_input}
+     ], Req, State}.
+
+content_types_provided(Req, State) ->
+    {[
+      {<<"application/json">>, res_to_json},
+      {undefined, res_to_json}
+     ], Req, State}.
+
+json_to_input(Req0, State0) ->
+    {ok, DataStr, Req1} = read_body(Req0),
+    Data = jsone:decode(DataStr),
+    State1 = State0#state{data=Data},
+    {Success, Response, Req2, State} = handle(Req1, State1),
+    BodyAsJson = jsone:encode(Response),
+    Req = cowboy_req:set_resp_body(BodyAsJson, Req2),
+    {Success, Req, State}.
+
+res_to_json(Req0, State0) ->
+    {_Success, Response, Req, State} = handle(Req0, State0),
+    BodyAsJson = jsone:encode(Response),
+    {BodyAsJson, Req, State}.
+
+
+%% internal
+read_body(Req0) ->
+    cowboy_req:read_body(Req0).
+
+handle(Req0, #state{op=[Mod, Fn]} = State0) ->
+    erlang:apply(Mod, Fn, [Req0, State0]).
+
+
+%% API
+say_hi(allowed_methods) -> [<<"GET">>].
+say_hi(Req0, State0) ->
+    Success = true,
+    Response = <<"Hello get">>,
+    {Success, Response, Req0, State0}.
